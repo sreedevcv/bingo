3664079bde1745aae51016b4606920eaacd3e166
Sreedev <sreedevcv0@gmail.com>
2023-03-19 11:02:43 +0530

    created the server class

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from graphics import Window
 from card import Bingo
+from server import BingoServer
+# bingo = Bingo(6)
+# window = Window(bingo)
 
-bingo = Bingo(6)
-window = Window(bingo)
+def hostGame():
+    server = BingoServer(game_size=5, player_count=2)
+    thread = server.initGame()
+    thread.start()
+
+
+    thread.join()
+
+hostGame()
